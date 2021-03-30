d464eb6a05dbc34c92213c40eb8bb58e92c4ec74
Nikita Bryskin <65920166+nikitabryskin@users.noreply.github.com>
2021-03-30 21:02:05 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,4 @@
-
+var time = setInterval(function() {
+  var date = new Date();
+  document.getElementByClass("Time").innerHTML = (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " по Москве/Минску");
+}, 1000);
